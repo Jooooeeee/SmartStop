e4252f2588f0612fdb9f389b6d71b76555d40e83
Joe <1454239240@qq.com>
2017-12-08 15:02:24 +0800

    2nd commit

--- app/src/main/java/com/example/joe/smartstop/MainActivity.java
+++ app/src/main/java/com/example/joe/smartstop/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.joe.smartstop;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
