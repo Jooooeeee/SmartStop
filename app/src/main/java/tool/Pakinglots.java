95fc62033dc5edbc673d00bd7d8964e6aa8be958
Joe <1454239240@qq.com>
2017-12-09 11:36:17 +0800

    4nd commit

---
+++ app/src/main/java/tool/Pakinglots.java
@@ -0,0 +1,53 @@
+package tool;
+
+/**
+ * Created by joe on 2017/12/9.
+ */
+
+public class Pakinglots {
+    private String name;
+    private String passWord;
+    private String licencePlate;
+    private String account;
+    private int phoneName
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public int getPhoneName() {
+        return phoneName;
+    }
+
+    public void setPhoneName(int phoneName) {
+        this.phoneName = phoneName;
+    }
+}
